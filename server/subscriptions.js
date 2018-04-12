45df43db0e545d0019f1cc758093ad3d6ca8dec8
richarli <richarli@cisco.com>
2018-04-12 20:33:37 +0100

    Ok fine. I guess we're doing this the dirty way. Code repetition for everyone!

--- server/subscriptions.js
+++ server/subscriptions.js
@@ -13,6 +13,13 @@ Meteor.methods({
     },
 
     'get_volume': function () {
+        const jsxapi = require('jsxapi');
+
+        // Connect over ssh to a codec
+        const xapi = new jsxapi.connect('ssh://integrator@10.230.105.193', {
+            username: 'integrator',
+            password: 'integrator',
+        });
         // Set up a call
         xapi.status
             .get('Audio Volume').then((volume) => {
@@ -21,10 +28,24 @@ Meteor.methods({
     },
 
     'set_alert_message': function (new_msg) {
+        const jsxapi = require('jsxapi');
+
+        // Connect over ssh to a codec
+        const xapi = new jsxapi.connect('ssh://integrator@10.230.105.193', {
+            username: 'integrator',
+            password: 'integrator',
+        });
         xapi.command('UserInterface Message Alert Display', {Text: new_msg});
     },
 
     'hey_listen': function () {
+        const jsxapi = require('jsxapi');
+
+        // Connect over ssh to a codec
+        const xapi = new jsxapi.connect('ssh://integrator@10.230.105.193', {
+            username: 'integrator',
+            password: 'integrator',
+        });
         // Listen to feedback
         const off = xapi.event.on('Standby', (event) => {
             // ...
