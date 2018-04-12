5022084bd4dba699a751cad72ea19f8cc909d55a
richarli <richarli@cisco.com>
2018-04-12 19:23:28 +0100

    adding gitignore, buttons, etc etc...

--- server/main.js
+++ server/main.js
@@ -3,27 +3,18 @@ import {Meteor} from 'meteor/meteor';
 Meteor.startup(() => {
     const jsxapi = require('jsxapi');
 
-    console.log('doing a thing');
 // Connect over ssh to a codec
     const xapi = new jsxapi.connect('ssh://integrator@10.230.105.193', {
         username: 'integrator',
         password: 'integrator',
     });
 
-    // return "xapi";
-
-    console.log('doing a thing 2');
-
-    var volume = xapi.status.get('Audio Volume');
-    console.log(volume);
     // Set up a call
 
     xapi.status
-        .get('Audio Volume').then((volume) => { console.log(volume); });
-
-
-    console.log('doing a thing3');
-
+        .get('Audio Volume').then((volume) => {
+        console.log(volume);
+    });
 
     xapi.command('UserInterface Message Alert Display', {Text: "Something Something Darkside."});
     // Listen to feedback
