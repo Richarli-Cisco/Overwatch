1ccbcd9c89d1cd9edf856fd93fd921bad97780c0
richarli <richarli@cisco.com>
2018-04-13 18:55:31 +0100

    Final commit before the weekend.
    Need to test on another machine... seems when I delete bootstrap-settings.json, the site works (drop downs, add project etc.) but when it's present, these parts stop working.
    
    Can't figure it out...

--- client/lib/subscribe.js
+++ client/lib/subscribe.js
@@ -4,8 +4,6 @@ import {Template} from 'meteor/templating';
 Template.index.events({
     'submit form': function (event) {
         event.preventDefault();
-        console.log("Ping");
-        Meteor.call('login');
         Meteor.call('get_volume');
     }
 });
