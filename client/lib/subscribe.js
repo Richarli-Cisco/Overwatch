43933bd957d12614bcfa5e3cfe2bdb233e7ce09e
richarli <richarli@cisco.com>
2018-04-23 14:18:32 +0100

    Adding in Helen's code - basic functionality updated.

--- client/lib/subscribe.js
+++ client/lib/subscribe.js
@@ -5,5 +5,8 @@ Template.index.events({
     'submit form': function (event) {
         event.preventDefault();
         Meteor.call('get_volume');
+        Meteor.call('get_presence');
+        Meteor.call('get_count');
+        Meteor.call('set_alert_message');
     }
 });
